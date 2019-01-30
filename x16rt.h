5272e6be2382dda5317193666402ef4bffe27589
Andrei <andrei.kuts@gmail.com>
2019-01-30 16:02:32 +0300

    x16rt

--- x16rt.h
+++ x16rt.h
@@ -1,16 +1,19 @@
 #ifndef X16RT_H
 #define X16RT_H
 
- #ifdef __cplusplus
+#define TIME_MASK 0xffffff80
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
- #include <stdint.h>
+#include <stdint.h>
 
- void x16rt_hash(const char* input, char* output);
+void x16rt_hash(const char* input, char* output);
+void sha256d(unsigned char *hash, const unsigned char *data, int len); //sha2.c
 
- #ifdef __cplusplus
+#ifdef __cplusplus
 }
 #endif
 
- #endif
+#endif
